16ad4ee0030a17333bd089dc579abcffd454195d
Brian Beck <brian.beck@outlook.com>
2022-08-27 00:33:50 -0700

    Add GTS style and linting (#1)

--- src/commands/autokey.ts
+++ src/commands/autokey.ts
@@ -1,39 +1,29 @@
-import { ChatInputCommandInteraction } from 'discord.js'
-import { SlashCommandBuilder } from '@discordjs/builders';
-import { AutoKeyString } from 'puzzle-lib'
+import {ChatInputCommandInteraction} from 'discord.js';
+import {SlashCommandBuilder} from '@discordjs/builders';
+import {AutoKeyString} from 'puzzle-lib';
 
 const AutoKeyCommand = {
-    data: new SlashCommandBuilder()
-        .setName('autokey')
-        .setDescription('Autokey Cipher')
-        .addStringOption(option =>
-            option
-                .setName('input')
-                .setDescription('Input')
-                .setRequired(true)
-        )
-        .addStringOption(option =>
-            option
-                .setName('key')
-                .setDescription('Key')
-                .setRequired(true)
-        )
-        .addBooleanOption(option =>
-            option
-                .setName('encrypt')
-                .setDescription('Encrypt')
-                .setRequired(false)
-        ),
+  data: new SlashCommandBuilder()
+    .setName('autokey')
+    .setDescription('Autokey Cipher')
+    .addStringOption(option =>
+      option.setName('input').setDescription('Input').setRequired(true)
+    )
+    .addStringOption(option =>
+      option.setName('key').setDescription('Key').setRequired(true)
+    )
+    .addBooleanOption(option =>
+      option.setName('encrypt').setDescription('Encrypt').setRequired(false)
+    ),
 
-    async execute(interaction: ChatInputCommandInteraction) {
-        const input = interaction.options.getString('input')!;
-        const key = interaction.options.getString('key')!;
-        const encrypt = interaction.options.getBoolean('encrypt') || false;
+  async execute(interaction: ChatInputCommandInteraction) {
+    const input = interaction.options.getString('input')!;
+    const key = interaction.options.getString('key')!;
+    const encrypt = interaction.options.getBoolean('encrypt') || false;
 
-        const aks = new AutoKeyString(input, key)
-        await interaction.reply(encrypt ? aks.encrypt() : aks.decrypt());
-    }
+    const aks = new AutoKeyString(input, key);
+    await interaction.reply(encrypt ? aks.encrypt() : aks.decrypt());
+  },
 };
 
-export { AutoKeyCommand };
-
+export {AutoKeyCommand};
--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,19 +1,19 @@
-import { AutoKeyCommand } from './autokey';
-import { CaesarCommand } from './caesar';
-import { MorseCommand } from './morse';
-import { VigenereCommand } from './vigenere';
+import {AutoKeyCommand} from './autokey';
+import {CaesarCommand} from './caesar';
+import {MorseCommand} from './morse';
+import {VigenereCommand} from './vigenere';
 
 const ALL_COMMANDS = [
-    AutoKeyCommand,
-    CaesarCommand,
-    MorseCommand,
-    VigenereCommand
+  AutoKeyCommand,
+  CaesarCommand,
+  MorseCommand,
+  VigenereCommand,
 ];
 
 export {
-    ALL_COMMANDS,
-    AutoKeyCommand,
-    CaesarCommand,
-    MorseCommand,
-    VigenereCommand
+  ALL_COMMANDS,
+  AutoKeyCommand,
+  CaesarCommand,
+  MorseCommand,
+  VigenereCommand,
 };
